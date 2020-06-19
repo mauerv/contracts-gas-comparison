1376973943a8a9837e57986235cd22a538fd2852
Mauro <mauro@osn.global>
2020-06-19 00:09:46 -0300

    Add networks to truffle-compile

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,16 @@
 module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    },
+    test: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    },
+  },
   compilers: {
     solc: {
       version: "0.6.9",
